52c06c1ad54d35d19fca76c7008134fecaaa5c10
zurez <zurez4u@gmail.com>
2016-03-14 16:35:00 +0530

    added counter

--- zomato.py
+++ zomato.py
@@ -2,6 +2,8 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import json
 import requests
+from datum import DatumBox
+from collections import Counter
 api_base_url="https://developers.zomato.com/api/v2.1/"
 api_key="3fae2a3d5cbf4920b04ab01873a03fdb"
 class Zomato(object):
@@ -16,13 +18,42 @@ class Zomato(object):
 	def get_reviews(self):
 		api_url= api_base_url+"reviews"
 		rid=self.get_id()
-		url= api_url+"?res_id="+str(rid)
+		steps="0"
+		url= api_url+"?res_id="+str(rid)+"&start="+steps
 		header = {"User-agent": "curl/7.43.0", "Accept": "application/json", "user-key":api_key}
 		response=requests.get(url,headers=header)
-		return response.json()
+		total_Reviews=int(response.json()['reviews_count'])
+		steps=int(total_Reviews/5)
+		count=0
+		responses=[]
+		total_keywords=Counter({})
+		for x in range(0,steps):
+			url= api_url+"?res_id="+str(rid)+"&start="+str(count)
+			response=requests.get(url,headers=header)
+			responses.append(response.json())
+			words=""
+
+			for i in response.json()['user_reviews']:
+				words+=i['review']['review_text']
+			datum = DatumBox()
+			keywords=Counter(datum.get_keywords(words))
+			total_keywords+=keywords
+			print (total_keywords.most_common(20))
+			count+=5
+		return responses
+	def extract_text(self):
+		responses=self.get_reviews()
+		reviews=""
+		for x in responses:	
+			raw= i['user_reviews']
+			
+			for i in raw:
+				reviews+=i['review_text']
+		return reviews
+
 
 test_url="https://www.zomato.com/ncr/smokeys-bbq-and-grill-dlf-cyber-city-gurgaon"
 test= Zomato(test_url)
-print (test.get_reviews())
+test.get_reviews()
 
 		
